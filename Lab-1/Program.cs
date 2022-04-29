1c2718e4611f880f9caef327735415d982e283a9
Dawid Studziżba <Dawid.Studzizba@microsoft.wsei.edu.pl>
2022-04-29 09:47:24 +0200

    Lab-6 code

---
+++ Lab-1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek a = new Ulamek(5, 4);
+            Ulamek b = new Ulamek(1, 2);
+            Console.WriteLine(a - b);
+        }
+    }
+    public class Ulamek
+    {
+        private int Licznik { get; set; }
+        private int Mianownik { get; set; }
+
+        public Ulamek()
+        {
+
+        }
+        public Ulamek(int licznik, int mianownik)
+        {
+            if (mianownik == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.", nameof(mianownik));
+            }
+            this.Licznik = licznik;
+            this.Mianownik = mianownik;
+        }
+        public Ulamek(Ulamek ulamek)
+        {
+            this.Licznik = ulamek.Licznik;
+            this.Mianownik = ulamek.Mianownik;
+        }
+        public static Ulamek operator +(Ulamek a) => a;
+        public static Ulamek operator -(Ulamek a) => new Ulamek(-a.Licznik, a.Mianownik);
+        public static Ulamek operator +(Ulamek a, Ulamek b) => new Ulamek(a.Licznik * b.Mianownik + b.Licznik * a.Mianownik, a.Mianownik * b.Mianownik);
+        public static Ulamek operator -(Ulamek a, Ulamek b) => new Ulamek(a.Licznik * b.Mianownik - b.Licznik * a.Mianownik, a.Mianownik * b.Mianownik);
+        public static Ulamek operator *(Ulamek a, Ulamek b) => new Ulamek(a.Licznik * b.Licznik, a.Mianownik * b.Mianownik);
+        public static Ulamek operator /(Ulamek a, Ulamek b)
+        {
+            if (b.Licznik == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Ulamek(a.Licznik * b.Mianownik, b.Licznik * a.Mianownik);
+        }
+
+        public override string ToString()
+        {
+            return $"{Licznik} / {Mianownik}";
+        }
+    }
+}
--- Lab-6/Program.cs
+++ Lab-6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab_6
 {
@@ -6,7 +8,120 @@ namespace Lab_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var users = new List<User>()
+            {
+                new User { Name = "a", Role = "ADMIN", Age = 32,
+                    Marks = null},
+                new User { Name = "b", Role = "MODERATOR", Age = 29,
+                    Marks = null},
+                new User { Name = "c", Role = "TEACHER", Age = 45,
+                    Marks = null},
+                new User { Name = "a", Role = "STUDENT", Age = 20,
+                    Marks = RandomMarks()},
+                new User { Name = "h", Role = "STUDENT", Age = 19,
+                    Marks = RandomMarks()},
+                new User { Name = "i", Role = "STUDENT", Age = 19,
+                    Marks = RandomMarks()},
+                new User { Name = "z", Role = "STUDENT", Age = 22,
+                    Marks = RandomMarks()},
+                new User { Name = "d", Role = "STUDENT", Age = 25,
+                    Marks = RandomMarks()}
+            };
+            Console.WriteLine("Number of records in the list: " + users.Count);
+            Console.WriteLine("List of users: ");
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
+            var userName = users.Select(u => u.Name).ToList();
+            var sortedList = (from user in users orderby user.Name
+                              select user.Name).ToList();
+            Console.WriteLine("Sorted list of users: ");
+            foreach (var user in sortedList)
+            {
+                Console.WriteLine(user);
+            }
+            var roles = new List<User>()
+            {
+                new User { Role = "ADMIN" },
+                new User { Role = "MODERATOR" },
+                new User { Role = "TEACHER" },
+                new User { Role = "STUDENT" }
+            };
+            Console.WriteLine("Avaliabe user roles: ");
+            foreach (var role in roles)
+            {
+                Console.WriteLine(role.Role);
+            }
+            var userRoles = roles.Select(r => r.Role).ToList();
+            var admins = new List<User>();
+            var moderators = new List<User>();
+            var teachers = new List<User>();
+            var students = new List<User>();
+            foreach (var user in users)
+            {
+                if (user.Role == "ADMIN")
+                {
+                    admins.Add(user);
+                }
+                if (user.Role == "MODERATOR")
+                {
+                    moderators.Add(user);
+                }
+                if (user.Role == "TEACHER")
+                {
+                    teachers.Add(user);
+                }
+                if (user.Role == "STUDENT")
+                {
+                    students.Add(user);
+                }
+            }
+            Console.WriteLine("Admins: ");
+            foreach (var user in admins)
+            {
+                Console.WriteLine(user.Name);
+            }
+            Console.WriteLine("Moderators: ");
+            foreach (var user in moderators)
+            {
+                Console.WriteLine(user.Name);
+            }
+            Console.WriteLine("Teachers: ");
+            foreach (var user in teachers)
+            {
+                Console.WriteLine(user.Name);
+            }
+            Console.WriteLine("Students: ");
+            foreach (var user in students)
+            {
+                Console.WriteLine(user.Name);
+            }
+            var nullsNumber = 0;
+            foreach (var user in users)
+            {
+                if (user.Marks == null)
+                {
+                    nullsNumber++;
+                }
+            }
+            Console.WriteLine("List of no mark users: " + nullsNumber);
         }
+        static int[] RandomMarks()
+        {
+            var random = new Random();
+            var randomMarks = new int[5] {random.Next(1, 6), random.Next(1, 6),
+            random.Next(1, 6), random.Next(1, 6), random.Next(1, 6) };
+            return randomMarks;
+        }
+    }
+    public class User
+    {
+        public string Name { get; set; }
+        public string Role { get; set; } // ADMIN, MODERATOR, TEACHER, STUDENT
+        public int Age { get; set; }
+        public int[] Marks { get; set; } // zawsze null gdy ADMIN, MODERATOR lub TEACHER
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? RemovedAt { get; set; }
     }
 }
