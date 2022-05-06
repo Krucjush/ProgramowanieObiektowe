be57011f1dacf99126cb213f14e713a283636324
Dawid Studziżba <Dawid.Studzizba@microsoft.wsei.edu.pl>
2022-05-06 09:46:44 +0200

    Code done on lessons

---
+++ Lab_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+namespace Lab_7
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=PK1-15;Initial Catalog=projectdb;Integrated Security=True";
+            using (DataContext dataContext = new DataContext(connectionString))
+            {
+                Table<UserEntity> users = dataContext.GetTable<UserEntity>();
+                IQueryable<UserEntity> query = users
+                                               .Where(q => q.RemovedAt.HasValue == false);
+                foreach (var user in query)
+                {
+                    Console.WriteLine("{0} {1}",user.Id, user.Name);
+                }
+            }
+        }
+    }
+}
---
+++ Lab_7/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Linq.Mapping;
+
+namespace Lab_7
+{
+    [Table(Name = "Users")]
+    public class UserEntity
+    {
+        [Column(IsPrimaryKey = true, Name = "Id")]
+        public long Id { get; set; }
+        [Column(Name = "Name")]
+        public string Name { get; set; }
+        [Column(Name = "Role")]
+        public string Role { get; set; }
+        [Column(Name = "CreatedAt")]
+        public DateTime? createdAd { get; set; }
+        [Column(Name ="RemovedAt")]
+        public DateTime? RemovedAt { get; set; }
+    }
+}
