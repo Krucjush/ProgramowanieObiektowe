7bcf45141b223675f8ce3fc8adf7ab81f2c2bfcb
Dawid Studziżba <Dawid.Studzizba@microsoft.wsei.edu.pl>
2022-05-13 09:44:16 +0200

    Lab-7 source code

--- Lab_7/Program.cs
+++ Lab_7/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.Linq;
 
 namespace Lab_7
@@ -12,7 +9,7 @@ namespace Lab_7
     {
         static void Main(string[] args)
         {
-            string connectionString = @"Data Source=PK1-15;Initial Catalog=projectdb;Integrated Security=True";
+            string connectionString = @"Data Source=PK1-22;Initial Catalog=projectdb;Integrated Security=True";
             using (DataContext dataContext = new DataContext(connectionString))
             {
                 Table<UserEntity> users = dataContext.GetTable<UserEntity>();
